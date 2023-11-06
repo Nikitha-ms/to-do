c9656e9d9633442dd4da9932bbd62915a288c28e
nshet <nikitha.shet@nokia.com>
2023-11-06 19:19:52 +0530

    static layout of todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,45 @@
-export const App = () => {
+import "./App.css";
+
+const App = () => {
   return (
-    <div>Hello</div>
-  )
-}
+    <div className="container">
+      <h1 className="heading">To Do List</h1>
+      <div className="flex-row">
+        <input type="text" className="input" placeholder="add item..." />
+        <button className="btn btn-add">add</button>
+      </div>
+      <ul className="list">
+        <li className="list-item">
+          <span className="task">task1</span>
+          <div className="flex-row">
+            <button className="btn btn-list">edit</button>
+            <button className="btn btn-list">delete</button>
+          </div>
+        </li>
+        <li className="list-item">
+          <span className="task">task2</span>
+          <div className="buttons">
+            <button className="btn btn-list">edit</button>
+            <button className="btn btn-list">delete</button>
+          </div>
+        </li>
+        <li className="list-item">
+          <span className="task">task3</span>
+          <div className="buttons">
+            <button className="btn btn-list">edit</button>
+            <button className="btn btn-list">delete</button>
+          </div>
+        </li>
+        <li className="list-item">
+          <span className="task">task4</span>
+          <div className="buttons">
+            <button className="btn btn-list">edit</button>
+            <button className="btn btn-list">delete</button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default App;
